3c28de5c91059aec6421971bc61ca3c0b62f7bf6
Jose D. Romero <josedanilromero@gmail.com>
2024-06-21 17:26:02 -0500

    feat(TimesTablesGUI): Add attributes to TimesTablesGUI class

--- TimesTablesGUI.java
+++ TimesTablesGUI.java
@@ -5,5 +5,13 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class TimesTablesGUI extends JFrame {
-    
+    private JTextField nameField;
+    private JButton startButton, submitButton, resetButton;
+    private JLabel instructionLabel, questionLabel, resultLabel;
+    private JTextField answerField;
+    private int correctCount;
+    private ArrayList<Integer> wrongList;
+    private int[][] questions;
+    private int currentQuestionIndex;
+    private String name;
 }
